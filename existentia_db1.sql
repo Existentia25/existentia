bb2ab080e9c616ec2b8640a511ebae053fed03c0
Existentia25 <128977658+Existentia25@users.noreply.github.com>
2023-04-14 23:26:03 +0200

    Add files via upload

---
+++ existentia_db1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE about(
+                      id  INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                      about_title VARCHAR(255) NOT NULL,
+                      about_content TEXT NOT NULL,
+                      about_img VARCHAR(255) NOT NULL,
+                      about_img_alt_en VARCHAR(255) NOT NULL,
+                      about_img_alt_pt VARCHAR(255) NOT NULL,
+                      about_button VARCHAR(255) NOT NULL,
+                      about_button_href VARCHAR(255) NOT NULL
+
+);
