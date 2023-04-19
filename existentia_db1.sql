793b44977152d71aad78c1ee8c5e2900b4ebbd45
Existentia25 <128977658+Existentia25@users.noreply.github.com>
2023-04-19 11:05:56 +0200

    Update existentia_db1.sql

--- existentia_db1.sql
+++ existentia_db1.sql
@@ -9,3 +9,50 @@ CREATE TABLE about(
                       about_button_href VARCHAR(255) NOT NULL
 
 );
+
+
+
+CREATE TABLE faq(
+                       id_faq INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                       faq_question_en VARCHAR(255),
+                       faq_question_pt VARCHAR(255),
+                       faq_answer_en TEXT,
+                       faq_answer_pt TEXT
+
+);
+
+CREATE TABLE blog(
+                       id_blog INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                       blog_img VARCHAR(255),
+                       blog_img_alt_en VARCHAR(255),
+                       blog_img_alt_pt VARCHAR(255),
+                       blog_title_en VARCHAR(255),
+                       blog_title_pt VARCHAR(255),
+                       blog_author VARCHAR(255),
+                       blog_date_time DATETIME,
+                       blog_category_en VARCHAR(255),
+                       blog_category_pt VARCHAR(255),
+                       blog_readmore_en VARCHAR(255),
+                       blog_readmore_pt VARCHAR(255),
+                       blog_content_en VARCHAR(255),
+                       blog_content_pt VARCHAR(255),
+                       blog_button VARCHAR(255),
+                       blog_button_href_en VARCHAR(255),
+                       blog_button_href_pt VARCHAR(255)
+                    
+);
+
+CREATE TABLE testimony(
+                       id_testimony INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                       testimony_title_en VARCHAR(255),
+                       testimony_title_pt VARCHAR(255),
+                       testimony_content_en VARCHAR(255),
+                       testimony_content_pt VARCHAR(255),
+                       testimony_name_en VARCHAR(255),
+                       testimony_name_pt VARCHAR(255),
+                       testimony_img VARCHAR(255),
+                       testimony_img_alt_en VARCHAR(255),
+                       testimony_img_alt_pt VARCHAR(255)
+                       
+                       
+)
